259bdd41e54e05d7c081334d667582804f5bbb72
PastryCoder12 <161971136+PastryCoder12@users.noreply.github.com>
2024-06-09 10:48:51 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var a = "Hello"
+var b = "World"
+
+console.log(a);
+console.log(b);
+console.log(a.concat(" ", b));
+
+var num1 = 5;
+var num2 = 6;
+
+console.log(num1);
+console.log(num2);
+console.log(num1 * num2)
+console.log(num1 + num2);
